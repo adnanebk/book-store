99c1355510540ee3a1227b9d4a6bee85be650763
adnanebk <benkouider.adnane@gmail.com>
2022-02-07 10:28:47 +0100

    added entities : book , comment

---
+++ src/main/java/com/cloud_steam/bookstore/models/Book.java
@@ -0,0 +1,28 @@
+package com.cloud_steam.bookstore.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Id
+    private UUID id;
+    private String name;
+    @OneToMany
+    private Set<Comment> comments;
+}
---
+++ src/main/java/com/cloud_steam/bookstore/models/Comment.java
@@ -0,0 +1,26 @@
+package com.cloud_steam.bookstore.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+
+@Entity
+@Getter
+@Setter
+public class Comment {
+
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Id
+    private UUID id;
+    private String text;
+}
