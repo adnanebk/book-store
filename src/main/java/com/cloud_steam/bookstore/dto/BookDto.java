594610726f7aaa47a01a25ef351ef4fcdd9f7cc1
adnanebk <benkouider.adnane@gmail.com>
2022-02-12 23:32:28 +0100

    refactor (convert classes to records)

--- src/main/java/com/cloud_steam/bookstore/dto/BookDto.java
+++ src/main/java/com/cloud_steam/bookstore/dto/BookDto.java
@@ -1,15 +1,13 @@
 package com.cloud_steam.bookstore.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 import java.util.Set;
 import java.util.UUID;
 
-@Getter
-@Setter
-public class BookDto {
-  private UUID id;
-  private String name;
-  private Set<CommentDto> comments;
-}
+
+public record BookDto (
+   UUID id,
+   String name,
+   Set<CommentDto> comments
+){}
--- src/main/java/com/cloud_steam/bookstore/dto/CommentDto.java
+++ src/main/java/com/cloud_steam/bookstore/dto/CommentDto.java
@@ -1,13 +1,8 @@
 package com.cloud_steam.bookstore.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 import java.util.UUID;
 
-@Getter
-@Setter
-public class CommentDto {
-  private UUID id;
-  private String text;
-}
+
+public record CommentDto(UUID id,String text) { }
